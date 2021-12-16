e9de8e2eef1447217d9496a5b3763043667c6924
srana17 <surajitrana@optum.com>
2021-12-17 00:55:36 +0530

    LeetCode 53. Maximum Subarray

--- arrays/max-subarray-sum.js
+++ arrays/max-subarray-sum.js
@@ -1,4 +1,16 @@
 /**
+ * LeetCode 53. Maximum Subarray
+ * Link: https://leetcode.com/problems/maximum-subarray/
+ * 
+ * Given an integer array nums, find the contiguous subarray (containing at least one number)
+ * which has the largest sum and return its sum.
+ * A subarray is a contiguous part of an array.
+ * Example 1: Input: nums = [-2,1,-3,4,-1,2,1,-5,4], Output: 6
+ * Example 2: Input: nums = [1], Output: 1
+ * Example 3: Input: nums = [5,4,-1,7,8], Output: 23
+ */
+
+/**
  * Find the maximum sub-array sum in the below array
  * @Input: [-5, 4, 6, -3, 4, -1]
  * @Output: 11
@@ -51,3 +63,12 @@ console.log(`The maximum sub-array sum by Brute-Force method is: ${maxSumBruteFo
 
 const maxSumKadanesAlgo = maxSubArraySumKadanesAlgorithm(input_array);
 console.log(`The maximum sub-array sum by Kadane's Algorithm is: ${maxSumKadanesAlgo}`);
+
+// Example 1: Input: nums = [-2,1,-3,4,-1,2,1,-5,4], Output: 6
+console.log(`Example 1:  Kadane's Algorithm is: ${maxSubArraySumKadanesAlgorithm([-2,1,-3,4,-1,2,1,-5,4])}`);
+
+// Example 2: Input: nums = [1], Output: 1
+console.log(`Example 2:  Kadane's Algorithm is: ${maxSubArraySumKadanesAlgorithm([1])}`);
+
+// Example 3: Input: nums = [5,4,-1,7,8], Output: 23
+console.log(`Example 3:  Kadane's Algorithm is: ${maxSubArraySumKadanesAlgorithm([5,4,-1,7,8])}`);
