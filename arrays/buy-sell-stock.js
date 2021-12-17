8f8b6a7780527d29ca5e23ddec07ec9a6f04b809
srana17 <surajitrana@optum.com>
2021-12-17 12:05:37 +0530

    LeetCode 121. Best Time to Buy and Sell Stock

--- arrays/buy-sell-stock.js
+++ arrays/buy-sell-stock.js
@@ -1,6 +1,6 @@
 /**
  * LeetCode 121. Best Time to Buy and Sell Stock
- * Link: https://leetcode.com/problems/maximum-subarray/
+ * Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
  *
  * You are given an array prices where prices[i] is the price of a given stock on the ith day.
  * You want to maximize your profit by choosing a single day to buy one stock and choosing a
