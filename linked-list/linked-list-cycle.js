57f8a2cf2cd2b1555932a4ee0a48ab4b34437ebf
surajitrana1985 <surajit.cool@gmail.com>
2021-12-22 21:20:46 +0530

    LeetCode 141. Linked List Cycle

--- linked-list/linked-list-cycle.js
+++ linked-list/linked-list-cycle.js
@@ -1,8 +1,12 @@
 /**
  * LeetCode 141. Linked List Cycle
- * Link: https://leetcode.com/problems/reverse-linked-list/
+ * Link: https://leetcode.com/problems/linked-list-cycle/
  *
- * Given the head of a singly linked list, reverse the list, and return the reversed list.
+ * Given head, the head of a linked list, determine if the linked list has a cycle in it.
+ * There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer.
+ * Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+ * 
+ * Return true if there is a cycle in the linked list. Otherwise, return false.
  *
  * Example 1: Input: head = [3, 2, 0, -4], pos = 1, Output: true, 
  * Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
